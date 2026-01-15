591a9b8875c6d2bfc9111b00c4cd2989eb101e30
HakuryuTB <jonathan.delvosal@edu.technobel.be>
2026-01-15 16:23:32 +0100

    fin de journée 15-01-26

--- src/app/O-Monde/proximity/proximity.ts
+++ src/app/O-Monde/proximity/proximity.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,5 +9,23 @@ import { RouterModule } from '@angular/router';
   styleUrl: './proximity.scss'
 })
 export class Proximity {
+
+
+  _client = inject(HttpClient)
+
+  monsters: any[] = [];
+
+  constructor() {
+    this.runRandomLoop();    
+  }
+
+  runRandomLoop(): void {
+      const randomCount = Math.floor(Math.random() * 10) + 1; 
+
+      for(let i = 0; i <randomCount; i++) {
+      this?._client.get('http://localhost:5050/api/monster/Random Level?lvlMin=1&lvlMax=5').subscribe(data => this.monsters.push(data))
+    }
+}
+
 
 }
