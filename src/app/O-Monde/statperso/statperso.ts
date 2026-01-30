da7221b245264ad99a6eb70cc6c728eeae576f94
HakuryuTB <jonathan.delvosal@edu.technobel.be>
2026-01-30 15:55:59 +0100

    30-01-26 correction des CSS et mise en page statperso + récup api

--- src/app/O-Monde/statperso/statperso.ts
+++ src/app/O-Monde/statperso/statperso.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeroService } from '../../Services/hero-service';
 import { PCSheet } from '../../models/PCSheet';
@@ -14,8 +13,14 @@ export class Statperso {
 
   _heroService = inject(HeroService)
 
-  pcSheet: PCSheet = this._heroService.pcSheet;
+  pcSheet = this._heroService.pcSheet;
 
-  
+  HpMax = computed(() => (this.pcSheet()?.hpMax ?? 0));
+
+  ManaMax = computed(() => (this.pcSheet()?.manaMax ?? 0));
+
+  ResFire = computed(() => (this.pcSheet()?.resFire ?? 0));
+  ResIce = computed(() => (this.pcSheet()?.resIce ?? 0));
+  ResLightning = computed(() => (this.pcSheet()?.resLightning ?? 0));
 
 }
--- src/app/Services/hero-service.ts
+++ src/app/Services/hero-service.ts
@@ -1,23 +1,35 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { PCSheet } from '../models/PCSheet';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-_client = inject(HttpClient)
+  _client = inject(HttpClient)
+  _router = inject(Router)
 
-pcSheet!: PCSheet;
-  
-constructor() {
-    this._client.get<PCSheet>(environment.ApiUrl + '/Character/GetPCStat').subscribe(data => {
-      this.pcSheet = data;
-    })
+  pcSheet = signal<PCSheet|null>(null);
 
-
-    
+  loadPCSheet(id: number) {
+    this._client.get<PCSheet>(environment.ApiUrl + '/Character/GetPCStat/' + id, {
+      headers:
+      {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    }).subscribe({
+        next: data => {
+          this.pcSheet.set(data);
+        },
+        error: err => {
+          console.error('Error fetching PCSheet:', err);
+          this._router.navigate(['/logscreen']);
+        }
+      })
   }
-  
+
 }
+
+
